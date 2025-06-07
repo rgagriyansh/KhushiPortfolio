80da8dc5b0162f9c3979cb62f2239c5ef4f68b7a
rgagriyansh <126491373+rgagriyansh@users.noreply.github.com>
2025-06-08 02:51:07 +0530

    Update ScrollProgress.tsx

--- app/components/ScrollProgress.tsx
+++ app/components/ScrollProgress.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { motion, useScroll, useSpring } from 'framer-motion';
 
 export const ScrollProgress = () => {
